b8e34c56fd9fd2ea3168bedf4a5df8f4d3292f69
Anna Kucherenko <kucherenko.place@icloud.com>
2023-02-21 07:55:50 +0200

    upd task 2
    task 2 done

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,29 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+// old
+
+// const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+
+// const liArr = [];
+// for (const ingridient of ingredients) {
+//   const el = document.createElement('li');
+//   el.textContent = ingridient;
+//   el.classList.add('item');
+//   liArr.push(el);
+// }
+// const ingredientsEl = document.querySelector('#ingredients');
+// ingredientsEl.append(...liArr);
+
+
+// new
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+const ingredientsEl = document.querySelector('#ingredients');
+
+const elements = ingredients.map(ingridient => {
+  const el = document.createElement('li');
+  el.textContent = ingridient;
+  el.classList.add('item');
+  return el;
+})
+console.log(elements);
+ingredientsEl.append(...elements);
