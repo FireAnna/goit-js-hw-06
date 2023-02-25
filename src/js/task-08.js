dc81d852237532b7fbc904d4d417216459b633cf
Anna Kucherenko <kucherenko.place@icloud.com>
2023-02-25 11:31:29 +0200

    task 8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,20 @@
 
+
+// preventDefault - скачування дій браузера за замовчуванням 
+const form = document.querySelector('.login-form');
+
+const onFormSubmit = event => {
+  event.preventDefault();
+  const formData = event.currentTarget.elements;
+  const userData = {};
+  for (let i = 0; i < formData.length - 1; i++) {
+    if (formData[i].value === '') {
+      return alert('Усі поля повинні бути заповнені!');
+    }
+    userData[formData[i].name] = formData[i].value;
+  }
+  console.log(userData);
+  event.currentTarget.reset();
+  return userData;
+};
+
